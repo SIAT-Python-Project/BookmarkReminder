baea0578464b75923cefc25515e52583552bab4c
hg_yellow <jang010505@gmail.com>
2024-05-23 15:07:45 +0900

    feat: #1 Add User CRUD

--- src/main/java/user/entity/User.java
+++ src/main/java/user/entity/User.java
@@ -1,6 +1,7 @@
 package user.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import bookmark.entity.Bookmark;
@@ -10,16 +11,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@ToString
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,5 +42,11 @@ public class User {
     private LocalDateTime createdDate;
 
     @OneToMany(mappedBy = "user")
-    private List<Bookmark> bookMarks;
+    private List<Bookmark> bookMarks = new ArrayList<>();
+
+    public void change(User newUser) {
+        if (newUser.password != null) {
+            this.password = newUser.password;
+        }
+    }
 }
---
+++ src/main/java/user/repository/UserRepository.java
@@ -0,0 +1,59 @@
+package user.repository;
+
+import jakarta.persistence.EntityManager;
+import user.entity.User;
+
+public class UserRepository {
+
+
+    // insert User
+    public static User insert(EntityManager em, User user) {
+        em.persist(user);
+        return user;
+    }
+
+    // read User
+    public static User findUserByUserId(EntityManager em, Long id) {
+        String sql = "SELECT u FROM User u WHERE u.id = :id";
+
+        return em.createQuery(sql, User.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
+    public static User findUserByNickname(EntityManager em, String nickname) {
+        String sql = "SELECT u FROM User u WHERE u.nickname = :nickname";
+
+        return em.createQuery(sql, User.class)
+                .setParameter("nickname", nickname)
+                .getSingleResult();
+    }
+
+    public static User findUserByEmail(EntityManager em, String email) {
+        String sql = "SELECT u FROM User u WHERE u.email = :email";
+
+        return em.createQuery(sql, User.class)
+                .setParameter("email", email)
+                .getSingleResult();
+    }
+
+    public static User findUserByLoginId(EntityManager em, String loginId) {
+        String sql = "SELECT u FROM User u WHERE u.loginId = :loginId";
+
+        return em.createQuery(sql, User.class)
+                .setParameter("loginId", loginId)
+                .getSingleResult();
+    }
+
+    public static void update(EntityManager em, Long id, User newUser) {
+        User targetUser = findUserByUserId(em, id);
+
+        targetUser.change(newUser);
+    }
+
+    public static void delete(EntityManager em, Long id) {
+        User targetUser = findUserByUserId(em, id);
+
+        em.remove(targetUser);
+    }
+}
