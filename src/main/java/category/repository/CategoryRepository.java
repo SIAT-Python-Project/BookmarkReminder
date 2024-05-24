26cdf33845483b8e419e07f38e87d1cb9fedb35b
hyejin <jesuisambitieux@gmail.com>
2024-05-24 10:09:28 +0900

    🐛fix: Add #5 Category Repository 변경2

--- src/main/java/category/repository/CategoryRepository.java
+++ src/main/java/category/repository/CategoryRepository.java
@@ -1,36 +1,28 @@
 package category.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import category.entity.Category;
-import common.util.DbUtil;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
 
 public class CategoryRepository {
 	
 	// category_id를 이용하여 Category 찾기
-	public static Category findCategoryById(EntityManager em, Long categoryId){
-		String sql = "SELECT c FROM Category c WHERE c.categoryId = :categoryId";
-		Category foundCategory = em.createQuery(sql, Category.class)
-								   .setParameter("categoryId", categoryId)
-								   .getSingleResult();
-		
-		return foundCategory;
+	public static Category findCategoryById(EntityManager em, Long categoryId) {
+		Category category = em.find(Category.class, categoryId);
+		return category;
 	}
 	
 	// category_name을 이용하여 Category 존재 여부 확인
-	public static boolean existCategoryByName(EntityManager em, String categoryName) {
+	public static Category existCategoryByName(EntityManager em, String categoryName) {
 		String sql = "SELECT c FROM Category c WHERE c.categoryName = :categoryName";
-		Category existCategory = em.createQuery(sql, Category.class)
-								 .setParameter("categoryName", categoryName.toUpperCase())
-								 .getSingleResult();
+		Category category = em.createQuery(sql, Category.class)
+				 			.setParameter("categoryName", categoryName.toUpperCase())
+				 			.getSingleResult();
 		
-		if(existCategory != null) {
-				return true;
-		}
-
-		return false;
+		return category;
 	}
 	
 	// 모든 Category 찾기
@@ -46,19 +38,9 @@ public class CategoryRepository {
 		em.persist(newCategory);
 	}
 	
-	// Category 수정
-	public static void updateCategory(EntityManager em, Long categoryId, String newCategoryName) {
-		Category changeCategory = findCategoryById(em, categoryId);
-		
-		if(changeCategory != null) {
-			changeCategory.changeCategoryName(newCategoryName);
-		}
-	}
-	
 	// Category 삭제
-	public static void deleteCategoryById(EntityManager em, Long categoryId) {
-		Category deletedCategory = em.find(Category.class, categoryId);
-		em.remove(deletedCategory);
+	public static void deleteCategoryById(EntityManager em, Category category) {
+		em.remove(category);
 	}
 	
 	
