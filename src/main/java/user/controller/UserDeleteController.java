37407609b5d62b013d472546f18bb820649483cc
hg_yellow <jang010505@gmail.com>
2024-05-27 15:42:34 +0900

    feat: Add #65 user delete button

---
+++ src/main/java/user/controller/UserDeleteController.java
@@ -0,0 +1,8 @@
+package user.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/userDelete.do")
+public class UserDeleteController extends HttpServlet {
+}
